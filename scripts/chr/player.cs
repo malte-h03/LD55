065d6f6ca8ff6640afe56c234aba03a14fec0cce
Mr_m <malte.hedenstrom@outlook.com>
2024-04-13 14:33:25 +0200

    Added grabby hands

--- scripts/chr/player.cs
+++ scripts/chr/player.cs
@@ -10,7 +10,7 @@ public partial class player : CharacterBody2D
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("LEFT", "RIGHT", "UP", "DOWN");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * movementSpeed;
