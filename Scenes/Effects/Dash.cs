b4ff63ebddbdbef71a090b51b0a73779f5c060de
Peralllax <166862561+Peralllax@users.noreply.github.com>
2024-04-13 21:32:29 +0200

    Dash effect added

---
+++ Scenes/Effects/Dash.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Dash : Node2D
+{
+	player playerRef;
+	
+	[Export] Timer dashTimer;
+	[Export] CpuParticles2D particle;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		playerRef = (player) GetTree().GetFirstNodeInGroup("Player");
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 directionVector = playerRef.Velocity.Normalized();
+		GlobalRotation = MathF.Atan2(directionVector.Y, directionVector.X) + Mathf.Pi;
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (Input.IsActionJustPressed("DASH"))
+		{
+			particle.Emitting = true;
+			GD.Print("Dashing");
+
+			dashTimer.Start(dashTimer.WaitTime);
+		}
+	}
+	
+	public void _on_dash_timer_timeout()
+	{
+		GD.Print("dash ready");
+	}
+}
+
